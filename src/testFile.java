809944301b57b1fa33ba6bbe0097a0bda54b8b9e
Sunny Sahu <s_sahu@dcunet.org>
2018-02-21 11:33:04 -0500

    first test

---
+++ src/testFile.java
@@ -0,0 +1,26 @@
+import org.openqa.selenium.WebDriver; 
+import org.openqa.selenium.firefox.FirefoxDriver;
+public class testFile {
+ 
+	public static void main(String[] args) throws InterruptedException {
+		
+		System.setProperty("webdriver.gecko.driver", "C:/Users/s_sahu/Downloads/geckodriver-v0.19.1-win64/geckodriver.exe");
+		//C:\Users\s_sahu\Downloads\geckodriver-v0.19.1-win64
+		
+		
+		
+		WebDriver driver1 = new FirefoxDriver();
+		
+      
+		driver1.get("http://www.dcu.org");
+ 
+        
+        System.out.println("Successfully opened the website of DCU");
+ 
+		
+		Thread.sleep(5000);
+		
+        
+        driver1.quit();
+    }
+}
